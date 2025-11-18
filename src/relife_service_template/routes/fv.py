bc40c9ac376fc8c66490e3339ffe4bea592cf374
Eric Wilczynski <eric.wilczynski@eurac.edu>
2025-11-17 23:27:58 -0500

    api update

---
+++ src/relife_service_template/routes/fv.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, HTTPException
+from relife_service_template.models.fv import FVRequest, FVResponse
+from relife_service_template.services.fv import calculate_fv
+from relife_service_template.auth.dependencies import get_authenticated_user_without_roles as get_current_user
+
+router = APIRouter(
+    prefix="/technical",
+    tags=["technical"],
+    responses={401: {"description": "Unauthorized"}},
+)
+
+@router.post("/fv", response_model=FVResponse, summary="Calculate FV weight")
+async def fv_endpoint(
+    request: FVRequest,
+    #user = Depends(get_current_user),
+):
+    """
+    Calculate the FV of Project.
+    """
+
+    try:
+       fv_value = calculate_fv(
+            ii_kpi=request.ii_kpi,
+            ii_min=request.ii_min,
+            ii_max=request.ii_max,
+            aoc_kpi=request.aoc_kpi,
+            aoc_min=request.aoc_min,
+            aoc_max=request.aoc_max,
+            irr_kpi=request.irr_kpi,
+            irr_min=request.irr_min,
+            irr_max=request.irr_max,
+            npv_kpi=request.npv_kpi,
+            npv_min=request.npv_min,
+            npv_max=request.npv_max,
+            profile=request.profile
+
+        )
+       return FVResponse(fv=fv_value, input=request)
+    except Exception as e:
+        # Return a 400 with the error message if something went wrong
+        raise HTTPException(status_code=400, detail=str(e))
---
+++ src/relife_service_template/routes/rei.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException
+from relife_service_template.models.rei import REIRequest, REIResponse
+from relife_service_template.services.rei import calculate_rei
+from relife_service_template.auth.dependencies import get_authenticated_user_without_roles as get_current_user
+
+router = APIRouter(
+    prefix="/financial",
+    tags=["financial"],
+    responses={401: {"description": "Unauthorized"}},
+)
+
+@router.post("/rei", response_model=REIResponse, summary="Calculate REI weight")
+async def rei_endpoint(
+    request: REIRequest,
+    #user = Depends(get_current_user),
+):
+    """
+    Calculate the REI of Project.
+    """
+
+    try:
+       rei_value = calculate_rei(
+            st_coverage_kpi=request.st_coverage_kpi,
+            st_coverage_min=request.st_coverage_min,
+            st_coverage_max=request.st_coverage_max,
+            onsite_res_kpi=request.onsite_res_kpi,
+            onsite_res_min=request.onsite_res_min,
+            onsite_res_max=request.onsite_res_max,
+            net_energy_export_kpi=request.net_energy_export_kpi,
+            net_energy_export_min=request.net_energy_export_min,
+            net_energy_export_max=request.net_energy_export_max
+            profile=request.profile
+
+        )
+       return REIResponse(rei=rei_value, input=request)
+    except Exception as e:
+        # Return a 400 with the error message if something went wrong
+        raise HTTPException(status_code=400, detail=str(e))
